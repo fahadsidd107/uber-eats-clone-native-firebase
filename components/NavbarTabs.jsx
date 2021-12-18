944579089b7840c08f913dbd1b214abc6d1f37ae
fahadsidd107 <fsiddiqui107@gmail.com>
2021-12-17 16:53:32 -0800

    Work Complete

---
+++ components/NavbarTabs.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-web";
+
+export default function NavbarTabs() {
+  const [activeTab, setActiveTab] = useState("Delivery");
+  return (
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        Text="Delivery"
+        // BtnColor="black"
+        // TextColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        Text="Pick up"
+        // BtnColor="white"
+        // TextColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </View>
+  );
+}
+const HeaderButton = (props) => (
+  <View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: props.activeTab == props.Text?'black':'white',
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 15,
+      }}
+      onPress={() => props.setActiveTab(props.Text)}
+    >
+      <Text
+        style={{
+          color: props.activeTab == props.Text?'white':'black',
+          fontSize: 15,
+          fontWeight: "900",
+        }}
+      >
+        {props.Text}
+      </Text>
+    </TouchableOpacity>
+  </View>
+);
---
+++ screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  Text, View,SafeAreaView } from 'react-native';
+import NavbarTabs from '../components/NavbarTabs';
+export default function Home() {
+    return (
+        <View>
+        <NavbarTabs/>
+        </View>
+          
+        
+    )
+}
