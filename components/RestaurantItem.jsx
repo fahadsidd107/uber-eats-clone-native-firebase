4a841fb863c9416e1e657cd2a05ff0c3e856deb3
fahadsidd107 <fsiddiqui107@gmail.com>
2021-12-24 00:28:30 +0500

    work complete

--- components/RestaurantItem.jsx
+++ components/RestaurantItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Image } from "react-native";
-
+import { TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 export default function RestaurantItem() {
   return (
     <View>
@@ -10,10 +11,16 @@ export default function RestaurantItem() {
 }
 
 const RestaurantImage = () => (
-  <Image
+  <><Image
     source={{
       uri: "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
     }}
-    style={{ width: "100%", height: 180 }}
-  />
+    style={{ width: "100%", height: 180 }} />
+    <TouchableOpacity style={{
+      position:'absolute',
+      right:20, 
+      top:20}}>
+      <MaterialCommunityIcons name='heart-outline' size={25} color='#fff'/>
+    </TouchableOpacity>
+    </>
 );
