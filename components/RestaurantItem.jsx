5ea937ca8e0c26913d44a4e2477499f8ec299e16
fahadsidd107 <fsiddiqui107@gmail.com>
2021-12-23 23:47:47 +0500

    work complete

--- components/RestaurantItem.jsx
+++ components/RestaurantItem.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React from "react";
+import { View, Image } from "react-native";
 
 export default function RestaurantItem() {
-    return (
-        <View>
-         
-        </View>
-    )
+  return (
+    <View>
+      <RestaurantImage />
+    </View>
+  );
 }
 
-const RestaurantImage=()=>{
-    <Image source={{uri: "https://source.unsplash.com/random/540×540/?restaurant"}}
-    style={{
-        width:'100%',
-        height:180,
+const RestaurantImage = () => (
+  <Image
+    source={{
+      uri: "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
     }}
-}
+    style={{ width: "100%", height: 180 }}
+  />
+);
