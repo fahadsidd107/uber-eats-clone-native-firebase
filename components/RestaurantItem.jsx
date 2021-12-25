d88ff628b0011a1abc035b9595b45732b982d6a7
fahadsidd107 <fsiddiqui107@gmail.com>
2021-12-25 19:23:26 +0500

    work complete

--- components/RestaurantItem.jsx
+++ components/RestaurantItem.jsx
@@ -1,14 +1,76 @@
 import React from "react";
 import { View, Image,Text} from "react-native";
 import { TouchableOpacity } from "react-native";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+export const localRestaurants = [
+  {
+    name: "Muhammadi Kabab House",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zameen.com%2Fblog%2F5-best-bbq-restaurants-lahore.html&psig=AOvVaw2niu7aEUayXC14zyI1SbVH&ust=1640527909168000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjJ9u2Q__QCFQAAAAAdAAAAABAD",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Sidra Fast Food",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pakistanihealthyrecipes.com%2Fhalwa-puri%2F&psig=AOvVaw3pKqYuwAshvtVs1lVqcB6T&ust=1640527961568000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDMxoiR__QCFQAAAAAdAAAAABAU",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "Huzaifa Burger Point",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F751890100264267957%2F&psig=AOvVaw0ZHGyqBaDf3Kp4WGEJeZpa&ust=1640528028304000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOij5K2R__QCFQAAAAAdAAAAABAD",
+    categories: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 700,
+    rating: 4.9,
+  },
+  {
+    name: "Muhammadi Sweets",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapercave.com%2Findian-sweets-wallpapers&psig=AOvVaw1G8lqfnB0qfsl9PJ7eptGV&ust=1640528152823000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiVmuOR__QCFQAAAAAdAAAAABAn",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "MashAllah Katakat Fish",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.epicurious.com%2Frecipes-menus%2F14-simple-easy-recipes-to-cook-fish-seafood-gallery&psig=AOvVaw1n0nwLZ-XwsSYfIJLD8HVR&ust=1640528455295000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjI5_OS__QCFQAAAAAdAAAAABAJ",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "Huzaifa Burger Point",
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.masala.tv%2Fgol-gappay-ki-chaat-mehboobs-kitchen-chef-mehboob-khan%2F%3Fref%3Dtheredish.com%252Fweb&psig=AOvVaw2exFB2-1M9KSryAFh3M2J2&ust=1640528502231000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDz14uT__QCFQAAAAAdAAAAABAN",
+    categories: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 700,
+    rating: 4.9,
+  },
+];
+
+
 export default function RestaurantItem() {
   return (
     <TouchableOpacity activeOpacity={1} style={{marginBottom:30,}} >
       <View style={{
       marginTop:10,
+      marginHorizontal:5,
       padding:15,
       backgroundColor:'white',
+      borderRadius:22,
       }}>
       <RestaurantImage />
       <RestaurantInfo />
@@ -55,7 +117,7 @@ const RestaurantInfo = () => (
   width:30,
   alignItems:'center',
   justifyContent:'center',
-  borderRadius:15,
+  borderRadius:16,
 }}>
 <Text>4.5</Text>
   </View>
