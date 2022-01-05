4040e9e8ad23257a1e696b9a02f41bf1378f7f57
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-05 23:27:31 +0500

    work complete

--- components/home/RestaurantItem.jsx
+++ components/home/RestaurantItem.jsx
@@ -63,23 +63,34 @@ export const localRestaurants = [
 ];
 
 
-export default function RestaurantItem(props) {
+export default function RestaurantItem({ navigation, ...props }) {
   return (
-    <TouchableOpacity activeOpacity={1} style={{marginBottom:30,}} >
-    {props.restaurantData.map((restaurant,index)=>(
-      <View key={index}
-      style={{
-      marginTop:10,
-      marginHorizontal:6,
-      padding:15,
-      backgroundColor:'white',
-      borderRadius:10,
-      }}>
-      <RestaurantImage image={restaurant.image_url}/>
-      <RestaurantInfo name={restaurant.name} rating={restaurant.rating}/>
-    </View>
+    <>
+    {props.restaurantData.map((restaurant, index) => (
+      <TouchableOpacity
+        key={index}
+        activeOpacity={1}
+        style={{ marginBottom: 30 }}
+        onPress={() =>
+          navigation.navigate("RestaurantDetail", {
+            name: restaurant.name,
+            image: restaurant.image_url,
+            price: restaurant.price,
+            reviews: restaurant.review_count,
+            rating: restaurant.rating,
+            categories: restaurant.categories,
+          })
+        }
+      >
+        <View
+          style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      </TouchableOpacity>
     ))}
-    </TouchableOpacity>
+  </>
   );
 }
 
