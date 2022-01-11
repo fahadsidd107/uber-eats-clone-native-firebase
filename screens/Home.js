62ead312c4b339a4b9e59ad963a1b1dec0a683da
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-11 23:01:53 +0500

    work complete

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { Divider } from "react-native-elements";
-import BottomTabs from "../components/home/BottomTabs";
-import Categories from "../components/home/Categories";
-import HeaderTabs from "../components/home/HeaderTabs";
+import BottomTabs from "../components/home/BottomTabs.jsx";
+import Categories from "../components/home/Categories.jsx";
+import HeaderTabs from "../components/home/HeaderTabs.jsx";
 import RestaurantItems, {
   localRestaurants,
 } from "../components/home/RestaurantItems";
