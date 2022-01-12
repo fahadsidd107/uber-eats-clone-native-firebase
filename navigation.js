4c68fe36d4aeb1ba73544c09a77150d665b65ea6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-12 07:42:48 +0500

    work complete

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import Home from "./screens/Home";
+import Home from "./screens/Home.jsx";
 import RestaurantDetail from "./screens/RestaurantDetail";
 import { Provider as ReduxProvider } from "react-redux";
 import configureStore from "./redux/store";
