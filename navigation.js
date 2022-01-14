7b6f8eec948e6c3a16c3e6652ab2042de854f8cc
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-15 03:03:53 +0500

    work complete

--- navigation.js
+++ navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home.jsx";
-import RestaurantDetail from "./screens/RestaurantDetail";
+import RestaurantDetail from "./screens/RestaurantDetail.jsx";
 import { Provider as ReduxProvider } from "react-redux";
 import configureStore from "./redux/store";
 import OrderCompleted from "./screens/OrderCompleted";
